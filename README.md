d480c888b4796c54ed88174f1f80463d742580c5
paul <hammerschmidtpaul@gmail.com>
2017-06-03 11:58:37 -0500

    updated readme, formatted output to denote whether solved or unsolved, unsolved puzzles have ones at unsolved spaces and probably isnt useful yet

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-### Sudoku Solver with String Output
-
+# Sudoku Solver with String Output
+1. Install sudoku npm module, instructions below.  Must have npm file manager already installed.
 [sudoku npm module](https://www.npmjs.com/package/sudoku)
+2. Run sudoku_string_maker.js from terminal to view strings of unsolved and solved puzzles.
+3. Modify the sudoku_string_maker.js to add more features as desired.
--- sudoku_string_maker.js
+++ sudoku_string_maker.js
@@ -10,12 +10,20 @@ var difficulty = sudoku.ratepuzzle(puzzle, 4);
 
 // console.log(typeof(solution[3]));
 
+var unsolvedString = ""
+
+for (var i = 0; i < puzzle.length; i++) {
+  unsolvedString += (puzzle[i] + 1).toString();
+}
+
 var answerString = ""
 
 for (var i = 0; i < solution.length; i++) {
   answerString += (solution[i] + 1).toString();
 }
 
-// console.log(answerString.length);
+console.log("Below is an unsolved puzzle string:\n");
+console.log(unsolvedString);
 
+console.log("\nBelow is a solved puzzle string:\n");
 console.log(answerString);
