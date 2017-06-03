2b5b723b4f31c8896861aed7bbcfaae04d01968a
paul <hammerschmidtpaul@gmail.com>
2017-06-03 12:52:02 -0500

    updated readme, unsolved puzzles represented with dashes, formatted script file

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Sudoku Solver with String Output
 ##### Install sudoku npm module
 * Must have npm file manager already installed.
-* Find the [sudoku npm module](https://www.npmjs.com/package/sudoku) here
+* Find the [sudoku npm module](https://www.npmjs.com/package/sudoku) instructions here.
+* Run the following command from the terminal.
+    
+    npm install sudoku
 ##### To view puzzles and solutions as strings
 * Run sudoku_string_maker.js from terminal
+
+    node sudoku_string_maker.js
 * Strings will output in terminal representing one long string of unsolved and solved puzzles.
+* Unsolved string has dashes (-) in place of unsolved spaces.
 ##### To add more features
-Modify the sudoku_string_maker.js script
+* Modify the sudoku_string_maker.js script
+* Send me an email at [hammerschmidtpaul@gmail.com](hammerschmidtpaul@gmail.com) to be added as a contributor.
--- sudoku_string_maker.js
+++ sudoku_string_maker.js
@@ -4,26 +4,28 @@ var puzzle = sudoku.makepuzzle();
 var solution   = sudoku.solvepuzzle(puzzle);
 var difficulty = sudoku.ratepuzzle(puzzle, 4);
 
-// console.log(puzzle);
-// console.log(solution);
-// console.log(difficulty);
-
-// console.log(typeof(solution[3]));
-
 var unsolvedString = ""
+var solutionString = ""
 
 for (var i = 0; i < puzzle.length; i++) {
-  unsolvedString += (puzzle[i] + 1).toString();
+  if (puzzle[i] != null) {
+    unsolvedString += (puzzle[i] + 1).toString();    
+  } else {
+    unsolvedString += "-";
+  }
 }
-
-var answerString = ""
 
 for (var i = 0; i < solution.length; i++) {
-  answerString += (solution[i] + 1).toString();
+  solutionString += (solution[i] + 1).toString();
 }
 
-console.log("Below is an unsolved puzzle string:\n");
+// Driver code below
+
+console.log("\nThe following puzzle has a relative difficulty of:");
+console.log(difficulty);
+
+console.log("\nBelow is an unsolved puzzle string:\n");
 console.log(unsolvedString);
 
-console.log("\nBelow is a solved puzzle string:\n");
-console.log(answerString);
+console.log("\nBelow is the corresponding solved puzzle string:\n");
+console.log(solutionString);
